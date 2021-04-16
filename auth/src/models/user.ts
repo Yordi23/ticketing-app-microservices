60594286e65fd92dbcd7e0bb7baf6df2ab8edcb2
Yordi Ogando <yordiogando@gmail.com>
2021-04-15 21:09:01 -0400

    Added type checking and static properties to User model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,22 @@
 import mongoose from 'mongoose';
 
+// This interface describe the properties needed to create a User
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+//This interface will describe the properties that a User model has
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// This interface describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,6 +28,10 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
