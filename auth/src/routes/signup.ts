888fb0cf05f54999cf26b5857524d005ed391646
Yordi Ogando <yordiogando@gmail.com>
2021-04-21 21:36:40 -0400

    Created validate request middleware and code refactor

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,43 +1,39 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+        const existingUser = await User.findOne({ email });
 
-    const { email, password } = req.body;
+        if (existingUser) {
+            throw new BadRequestError('Email already in use');
+        }
 
-    const existingUser = await User.findOne({ email });
+        const user = User.build({ email, password });
+        await user.save();
 
-    if (existingUser) {
-        throw new BadRequestError('Email already in use');
-    }
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
 
-    const user = User.build({ email, password });
-    await user.save();
+        req.session = {
+            jwt: userJwt
+        };
 
-    const userJwt = jwt.sign({
-        id: user.id,
-        email: user.email
-    }, process.env.JWT_KEY!);
-
-    req.session = {
-        jwt: userJwt
-    };
-
-    res.status(201).send(user);
-});
+        res.status(201).send(user);
+    });
 
 export { router as signupRouter };
