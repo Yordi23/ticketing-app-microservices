087d23d953716ef68a918abcebed8e1a48271016
Yordi Ogando <yordiogando@gmail.com>
2021-04-22 23:59:03 -0400

    Code refactor

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,54 +1,24 @@
-import 'express-async-errors';
-import express from 'express';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', () => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT is not defined');
-    }
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT is not defined');
+	}
 
-    try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        console.log('Connected to DB...');
-    } catch (error) {
-        console.error(error);
-    }
-    app.listen(3000, () => {
-        console.log('Listening on port 3000');
-    });
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Connected to DB...');
+	} catch (error) {
+		console.error(error);
+	}
+	app.listen(3000, () => {
+		console.log('Listening on port 3000');
+	});
 };
 
 start();
