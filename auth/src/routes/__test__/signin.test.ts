14767cc558af62752a778b06f4c53392faa94f8c
Yordi Ogando <yordiogando@gmail.com>
2021-04-25 20:18:39 -0400

    Added tests to signin

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns 400 when supplied non existing email', async () => {
+	return request(app)
+		.post('/api/users/signin')
+		.send({
+			email: 'test@test.com',
+			password: 'password',
+		})
+		.expect(400);
+});
+
+it('returns 400 when incorrect password is supplied', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password',
+		})
+		.expect(201);
+
+	await request(app)
+		.post('/api/users/signin')
+		.send({
+			email: 'test@test.com',
+			password: 'password2',
+		})
+		.expect(400);
+});
+
+it('returns 200  and a cookie when valid creadentials are provided password is supplied', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password',
+		})
+		.expect(201);
+
+	const response = await request(app)
+		.post('/api/users/signin')
+		.send({
+			email: 'test@test.com',
+			password: 'password',
+		})
+		.expect(200);
+
+	expect(response.get('Set-Cookie')).toBeDefined();
+});
