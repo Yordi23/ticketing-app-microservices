5adbdfb9b7a6a571e21aae1ba1778e140d27e7fa
Yordi Ogando <yordiogando@gmail.com>
2021-05-08 13:38:28 -0400

    Created new tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,28 @@
+import 'express-async-errors';
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import {
+	errorHandler,
+	NotFoundError,
+} from '@yd-ticketing-app/common';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+
+app.all('*', () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
