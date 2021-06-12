9f194bf53de17124e2b249a34c5fc9cf149a236c
Yordi Ogando <yordiogando@gmail.com>
2021-06-11 23:33:16 -0400

    Created preliminar test for create order endpoint

---
+++ orders/src/routes/__test__/create.test.ts
@@ -0,0 +1,58 @@
+import { OrderStatus } from '@yd-ticketing-app/common';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('returns an error if the ticket does not exist', async () => {
+	const ticketId = mongoose.Types.ObjectId();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({
+			ticketId,
+		})
+		.expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+	const ticket = Ticket.build({
+		price: 25,
+		title: 'concert',
+	});
+	await ticket.save();
+
+	const order = Order.build({
+		expiresAt: new Date(),
+		status: OrderStatus.CREATED,
+		ticket: ticket.id,
+		userId: mongoose.Types.ObjectId().toHexString(),
+	});
+	await order.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(400);
+});
+
+it('create an order and reserves a ticket', async () => {
+	const ticket = Ticket.build({
+		price: 25,
+		title: 'concert',
+	});
+	await ticket.save();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(201);
+});
---
+++ orders/src/test/setup.ts
@@ -0,0 +1,63 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+import jwt from 'jsonwebtoken';
+
+declare global {
+	namespace NodeJS {
+		interface Global {
+			signin(): string[];
+		}
+	}
+}
+
+jest.mock('../nats-wrapper.ts');
+
+let mongoMemoryServer: any;
+
+beforeAll(async () => {
+	process.env.JWT_KEY = 'secret';
+
+	mongoMemoryServer = new MongoMemoryServer();
+	const mongoUri = await mongoMemoryServer.getUri();
+
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+});
+
+beforeEach(async () => {
+	jest.clearAllMocks();
+
+	const collections = await mongoose.connection.db.collections();
+
+	for (let collection of collections) {
+		await collection.deleteMany({});
+	}
+});
+
+afterAll(async () => {
+	await mongoMemoryServer.stop();
+	await mongoose.connection.close();
+});
+
+global.signin = () => {
+	const id = mongoose.Types.ObjectId().toHexString();
+
+	const payload = {
+		id,
+		email: 'test@test.com',
+	};
+
+	const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+	const session = { jwt: token };
+
+	//This is for simulating the way the browser returns the cookies
+	const sessionJSON = JSON.stringify(session);
+	const base64Session = Buffer.from(sessionJSON).toString('base64');
+
+	return [`express:sess=${base64Session}`];
+};
