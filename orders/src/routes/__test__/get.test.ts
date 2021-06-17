4dc7f9fd20822f8aa3618dbae58ae03fd6751a14
Yordi Ogando <yordiogando@gmail.com>
2021-06-17 00:33:30 -0400

    Created get endpoint

---
+++ orders/src/routes/__test__/get.test.ts
@@ -0,0 +1,80 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { deleteOrderRouter } from '../delete';
+import mongoose from 'mongoose';
+
+const createTicket = async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 25,
+	});
+
+	return ticket.save();
+};
+
+it('returns the order with the provided id', async () => {
+	const ticket = await createTicket();
+
+	const user = global.signin();
+
+	const { body: order } = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user)
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(201);
+
+	const response = await request(app)
+		.get('/api/orders/' + order.id)
+		.set('Cookie', user)
+		.expect(200);
+
+	const expectedValue = {
+		...order,
+	};
+
+	delete expectedValue.ticket;
+
+	expect(response.body).toMatchObject(expectedValue);
+});
+
+it('returns an 404 code error if the provided id does not match any order', async () => {
+	const ticket = await createTicket();
+
+	const user = global.signin();
+
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', user)
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(201);
+
+	await request(app)
+		.get('/api/orders/' + mongoose.Types.ObjectId().toHexString())
+		.set('Cookie', user)
+		.expect(404);
+});
+
+it('returns an 401 code error if the provided id match an order related to another user', async () => {
+	const ticket = await createTicket();
+
+	const user1 = global.signin();
+	const user2 = global.signin();
+
+	const { body: order } = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user2)
+		.send({
+			ticketId: ticket.id,
+		})
+		.expect(201);
+
+	await request(app)
+		.get('/api/orders/' + order.id)
+		.set('Cookie', user1)
+		.expect(401);
+});
--- orders/src/routes/get.ts
+++ orders/src/routes/get.ts
@@ -1,14 +1,31 @@
-import { NotFoundError } from '@yd-ticketing-app/common';
+import {
+	NotAuthorizedError,
+	NotFoundError,
+	requireAuth,
+} from '@yd-ticketing-app/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:id', async (req: Request, res: Response) => {
-	// const ticket = await Ticket.findById(req.params.id);
-	// if (!ticket) {
-	// 	throw new NotFoundError();
-	// }
-	// res.status(200).send(ticket);
-});
+router.get(
+	'/api/orders/:orderId',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const order = await Order.findById(req.params.orderId).populate(
+			'ticket'
+		);
+
+		if (!order) {
+			throw new NotFoundError();
+		}
+
+		if (order.userId !== req.currentUser!.id) {
+			throw new NotAuthorizedError();
+		}
+
+		res.send(order);
+	}
+);
 
 export { router as getOrderRouter };
