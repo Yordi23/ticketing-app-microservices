5216e3e0afb1b9285eb2a65b075a85af56cd6f1e
Yordi Ogando <yordiogando@gmail.com>
2021-06-17 00:14:15 -0400

    Created get all endpoint

--- orders/src/routes/getAll.ts
+++ orders/src/routes/getAll.ts
@@ -1,11 +1,19 @@
-import { NotFoundError } from '@yd-ticketing-app/common';
+import { NotFoundError, requireAuth } from '@yd-ticketing-app/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/', async (req: Request, res: Response) => {
-	// const tickets = await Ticket.find({});
-	// res.status(200).send(tickets);
-});
+router.get(
+	'/api/orders',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const orders = await Order.find({
+			userId: req.currentUser!.id,
+		}).populate('ticket');
+
+		res.send(orders);
+	}
+);
 
 export { router as getAllOrdersRouter };
