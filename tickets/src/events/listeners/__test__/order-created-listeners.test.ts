c76599a912e7c876e42a570b3ced73397a96ff2a
Yordi Ogando <yordiogando@gmail.com>
2021-06-27 20:39:09 -0400

    OrderCreated listener implementation and test

---
+++ tickets/src/events/listeners/__test__/order-created-listeners.test.ts
@@ -0,0 +1,58 @@
+import {
+	OrderCreatedEvent,
+	OrderStatus,
+} from '@yd-ticketing-app/common';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created.listener';
+import { Types } from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+	const listener = new OrderCreatedListener(natsWrapper.client);
+
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+		userId: Types.ObjectId().toHexString(),
+	});
+
+	await ticket.save();
+
+	const data: OrderCreatedEvent['data'] = {
+		id: Types.ObjectId().toHexString(),
+		status: OrderStatus.CREATED,
+		ticket: {
+			id: ticket.id,
+			price: ticket.id,
+		},
+		userId: Types.ObjectId().toHexString(),
+		expiresAt: 'randomdate',
+		version: 0,
+	};
+
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn(),
+	};
+
+	return { listener, data, msg, ticket };
+};
+
+it('updates the orderId propertie of the ticket', async () => {
+	const { listener, data, msg, ticket } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	const updatedTicket = await Ticket.findById(data.id);
+
+	expect(updatedTicket?.orderId).toEqual(data.id);
+});
+
+it('acknowledges the message', async () => {
+	const { listener, data, msg } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
--- tickets/src/events/listeners/order-created.listener.ts
+++ tickets/src/events/listeners/order-created.listener.ts
@@ -2,8 +2,10 @@ import {
 	Listener,
 	Subjects,
 	OrderCreatedEvent,
+	NotFoundError,
 } from '@yd-ticketing-app/common';
 import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -11,6 +13,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 	queueGroupName = queueGroupName;
 
 	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+		const ticket = await Ticket.findById(data.ticket.id);
+
+		if (!ticket) {
+			throw new NotFoundError();
+		}
+
+		ticket.set({ orderId: data.id });
+
+		await ticket.save();
+
 		msg.ack();
 	}
 }
