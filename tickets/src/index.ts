8858e485b173acd5f50dfd0259161a7d1809a14c
Yordi Ogando <yordiogando@gmail.com>
2021-07-03 17:16:08 -0400

    Created expiration service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { OrderCancelledListener } from './events/listeners/order-cancelled.listener';
 import { OrderCreatedListener } from './events/listeners/order-created.listener';
 import { natsWrapper } from './nats-wrapper';
 
@@ -30,7 +31,7 @@ const start = async () => {
 		process.on('SIGTERM', () => natsWrapper.client.close());
 
 		new OrderCreatedListener(natsWrapper.client).listen();
-		new OrderCreatedListener(natsWrapper.client).listen();
+		new OrderCancelledListener(natsWrapper.client).listen();
 
 		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
